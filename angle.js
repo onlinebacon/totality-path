9338a295a45f420fde2f0d13dbc025684c9eace5
bacon <bacon.antife@gmail.com>
2024-04-14 05:22:57 -0300

    Accepts negative angles

--- angle.js
+++ angle.js
@@ -1,10 +1,16 @@
 const ANGLE = {
 	parse: (str) => {
+		str = str.trim();
+		let sign = 1;
+		if (str.startsWith('-')) {
+			str = str.substring(1).trim();
+			sign = -1;
+		}
 		const values = str.trim().split(/\s+/).map((value, i) => {
 			return value * (60 ** -i);
 		});
 		const sum = values.reduce((a, b) => a + b, 0);
-		return sum;
+		return sum * sign;
 	},
 };
 
